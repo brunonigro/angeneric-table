56d900c2b41724e5c0f08ecef32aa4b388e2361f
Bruno N. Campos <brunosopmac@gmail.com>
2017-07-27 06:49:11 -0300

    Install and config Generic Table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GtConfig } from '@angular-generic-table/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,81 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  public configObject: GtConfig<any>;
+
+  public data: Array<{
+    id: number,
+    name: string,
+    lucky_number: number
+  }> = [];
+
+  constructor() {
+
+    this.configObject = {
+      settings: [{
+        objectKey: 'id',
+        sort: 'desc',
+        columnOrder: 0
+      }, {
+        objectKey: 'name',
+        sort: 'enable',
+        columnOrder: 1
+      }, {
+        objectKey: 'lucky_number',
+        sort: 'enable',
+        columnOrder: 2
+      }],
+      fields: [{
+        name: 'Id',
+        objectKey: 'id'
+      }, {
+        name: 'Name',
+        objectKey: 'name'
+      }, {
+        name: 'Lucky number',
+        objectKey: 'lucky_number'
+      }],
+      data: [{
+        'id': 1,
+        'name': 'Anna',
+        'lucky_number': 63
+      }, {
+        'id': 2,
+        'name': 'Julie',
+        'lucky_number': 8
+      }, {
+        'id': 3,
+        'name': 'Lillian',
+        'lucky_number': 30
+      }, {
+        'id': 4,
+        'name': 'Norma',
+        'lucky_number': 13
+      }, {
+        'id': 5,
+        'name': 'Ralph',
+        'lucky_number': 28
+      }, {
+        'id': 6,
+        'name': 'Benjamin',
+        'lucky_number': 66
+      }, {
+        'id': 7,
+        'name': 'George',
+        'lucky_number': 66
+      }, {
+        'id': 8,
+        'name': 'Ryan',
+        'lucky_number': 65
+      }, {
+        'id': 9,
+        'name': 'Martha',
+        'lucky_number': 57
+      }, {
+        'id': 10,
+        'name': 'Todd',
+        'lucky_number': 65
+      }]
+    };
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { GenericTableModule } from '@angular-generic-table/core';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    GenericTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
